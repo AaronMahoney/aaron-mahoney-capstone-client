090b46bdb7c71272b6f99cef5597f84bfdd9de61
Aaron Mahoney <aaronmahoney1993@outlook.com>
2025-02-18 18:15:21 -0700

    header/footer built at mobile, a few pages with styling, links and routes established

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -2,10 +2,13 @@ import './MainPage.scss';
 
 function MainPage() {
   return (
-    <div>
-      
+    <div className="mainpage">
+      <section className="hero">
+        <h1 className="hero-header">The Rock</h1>
+        <h2 className="hero-subheader">Newfoundland and Labrador</h2>
+      </section>
     </div>
-  )
+  );
 }
 
 export default MainPage;
